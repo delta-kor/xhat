ba678910967c72a997af6374c6a0e7c5cb0c6425
delta-kor <deltakbot1@gmail.com>
2020-09-01 23:26:46 +0900

    Remove unused code

--- src/middlewares/view.ts
+++ src/middlewares/view.ts
@@ -4,7 +4,6 @@ export default class View {
   static mount(express: Application): Application {
     express.set('view engine', 'pug');
     express.set('views', 'views/pages');
-    express.locals.pretty = true;
     return express;
   }
 }
