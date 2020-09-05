4031a9647b8b85bf2df3314fe367bc15be80c1e6
delta-kor <deltakbot1@gmail.com>
2020-09-05 13:25:26 +0900

    Fix engine

--- src/providers/engine.ts
+++ src/providers/engine.ts
@@ -3,7 +3,20 @@ import fs from 'fs';
 export default function Engine(filePath: string, options: any, callback: any): any {
   fs.readFile(filePath, (err, data) => {
     if (err) callback(err);
-    const html = data.toString();
+
+    let html = data.toString();
+    const groups = html.match(/#([^"<]+)#/g);
+
+    if (groups) {
+      groups.forEach((value) => {
+        const blocks = value.split('||');
+        const tag = blocks[0].replace(/#/g, '');
+        const defaultValue = blocks[1].replace(/#/g, '');
+        const result = options[tag] || defaultValue;
+        html = html.replace(value, result);
+      });
+    }
+
     callback(null, html);
   });
 }
